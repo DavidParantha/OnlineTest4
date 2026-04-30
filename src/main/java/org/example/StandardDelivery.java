618997af7d2a0760386a1dcb325cf2508d6e1a34
David Parantha <150181814+DavidParantha@users.noreply.github.com>
2026-04-30 22:24:42 +0530

    Refactor StandardDelivery class formatting

--- src/main/java/org/example/StandardDelivery.java
+++ src/main/java/org/example/StandardDelivery.java
@@ -1,15 +1,12 @@
 package org.example;
 
 public class StandardDelivery extends Delivery {
-
     public StandardDelivery(String id, String name, double distance) {
         super(id, name, distance);
     }
-
     public double calculateCost() {
         return distance * 5;
     }
-
     public int getPriority() {
         return 1;
     }
